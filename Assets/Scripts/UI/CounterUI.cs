4ced0250c12ebfc4868d118ac84e55b0a45f5dce
nekt2111 <54265542+nekt2111@users.noreply.github.com>
2021-12-27 17:00:13 +0200

    ui update

--- Assets/Scripts/UI/CounterUI.cs
+++ Assets/Scripts/UI/CounterUI.cs
@@ -1,15 +1,23 @@
+using SteeringBehaviors.GameLoop;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SteeringBehaviors
 {
-    public class NewBehaviourScript : MonoBehaviour
+    public class CounterUI : MonoBehaviour
     {
+
+        [SerializeField] private Text _deerToKillText;
+        [SerializeField] private Text _rabbitsToKillText;
+        [SerializeField] private Text _wolvesToKillText;
+        private Score _score;
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            
         }
 
         // Update is called once per frame
@@ -18,17 +26,28 @@ namespace SteeringBehaviors
 
         }
 
-        public void Intialize(IKiller killer)
+        public void Intialize(Score score)
         {
-            killer.OnKill += HandleKill;
+            _score = score;
+            score.OnDeerKilled += () => HandleKill(Animal.Deer);
+            score.OnRabbitsKilled += () => HandleKill(Animal.Rabbit);
+            score.OnWolfKilled += () => HandleKill(Animal.Wolf);
         }
 
         private void HandleKill(Animal animal)
         {
-
+            switch (animal)
+            {
+                case Animal.Deer:
+                    _deerToKillText.text = $"Deers to kill {_score.DeerKilledAmount} / {_score.DeerToKillAmount}";
+                    break;
+                case Animal.Rabbit:
+                    _rabbitsToKillText.text =  $"Rabbits to kill {_score.RabbitsKilledAmount} / {_score.RabbitsToKillAmount}";
+                    break;
+                case Animal.Wolf:
+                    _wolvesToKillText.text = $"Wolves to kill {_score.WolvesKilledAmount} / {_score.WolvesToKillAmount}";
+                    break;
+            }
         }
-
-
     }
-
 }
